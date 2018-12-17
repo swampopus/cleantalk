3c105763eb99f49427cc3cee5bcbdeec1a4b544a
davydovct <davydov@cleantalk.org>
2018-12-17 16:22:11 +0500

    remove useless checking

--- lib/Drupal/cleantalk/Form/CleantalkCheckCommentsForm.php
+++ lib/Drupal/cleantalk/Form/CleantalkCheckCommentsForm.php
@@ -74,37 +74,34 @@ function cleantalk_check_comments_form_submit($form, &$form_state)
 	}
 }
 function cleantalk_find_spam_comments()
-{
-	if (variable_get('cleantalk_authkey', '') != '')
-	{		
-        // Get all comments
-        $comments = db_query("SELECT c.cid, c.uid, u.mail, c.name, c.subject, c.created, c.status FROM {comment} c INNER JOIN {users} u on c.uid = u.uid");
-		$data = array();
-		$spam_comments = array();	
-        foreach ($comments as $comment) 
-        {
-            // Skip adding the role to the user if they already have it.
-            if ($comment !== FALSE && isset($comment->mail)) 
-                array_push($data, $comment->mail);
-        }
-        $data=implode(',',$data);
-        $result=\Drupal\cleantalk\CleantalkHelper::api_method__spam_check_cms(trim(variable_get('cleantalk_authkey', '')), $data);	
-        if(isset($result['error_message']))
-            drupal_set_message($result['error_message'],'error');
-        else
-        {
-			foreach($result as $key => $value)
+{		
+    // Get all comments
+    $comments = db_query("SELECT c.cid, c.uid, u.mail, c.name, c.subject, c.created, c.status FROM {comment} c INNER JOIN {users} u on c.uid = u.uid");
+	$data = array();
+	$spam_comments = array();	
+    foreach ($comments as $comment) 
+    {
+        // Skip adding the role to the user if they already have it.
+        if ($comment !== FALSE && isset($comment->mail)) 
+            array_push($data, $comment->mail);
+    }
+    $data=implode(',',$data);
+    $result=\Drupal\cleantalk\CleantalkHelper::api_method__spam_check_cms(trim(variable_get('cleantalk_authkey', '')), $data);	
+    if(isset($result['error_message']))
+        drupal_set_message($result['error_message'],'error');
+    else
+    {
+		foreach($result as $key => $value)
+		{
+			if ($value['appears'] == '1' )
 			{
-				if ($value['appears'] == '1' )
+				foreach ($comments as $comment)
 				{
-					foreach ($comments as $comment)
-					{
-						if ($comment->mail == $key)
-							$spam_comments[] = $comment;
-					}
-				}              
-			}        	
-        }
-        return $spam_comments;	
-	}
+					if ($comment->mail == $key)
+						$spam_comments[] = $comment;
+				}
+			}              
+		}        	
+    }
+    return $spam_comments;	
 }
