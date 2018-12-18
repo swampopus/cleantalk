9350e354bbe35319981546cfc848ab436df29ebc
davydovct <davydov@cleantalk.org>
2018-12-18 10:43:58 +0500

    fix error

--- lib/Drupal/cleantalk/Form/CleantalkSettingsForm.php
+++ lib/Drupal/cleantalk/Form/CleantalkSettingsForm.php
@@ -119,7 +119,7 @@ function cleantalk_settings_form($form, &$form_state) {
 function cleantalk_settings_form_validate($form, &$form_state) { 
   if ($form_state['values']['cleantalk_authkey']){
     $cleantalk_auth_key = trim($form_state['values']['cleantalk_authkey']);
-    $is_valid = \Drupal\cleantalk\CleantalkHelper::api_method__notice_validate_key($cleantalk_auth_key));
+    $is_valid = \Drupal\cleantalk\CleantalkHelper::api_method__notice_validate_key($cleantalk_auth_key);
     if ($is_valid['valid'] === 1)
     {
       \Drupal\cleantalk\CleantalkHelper::api_method_send_empty_feedback($cleantalk_auth_key, CLEANTALK_USER_AGENT);
