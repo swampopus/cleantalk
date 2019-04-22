9f194e141e9f8297fd32977a7f634361b9cc0a33
davydov <artem@ip-192-168-0-101.is74.loc>
2019-04-22 16:16:01 +0500

    update helper

--- src/CleantalkHelper.php
+++ src/CleantalkHelper.php
@@ -52,97 +52,139 @@ class CleantalkHelper
 	);
 	
 	public static $private_networks = array(
-		'10.0.0.0/8',
-		'100.64.0.0/10',
-		'172.16.0.0/12',
-		'192.168.0.0/16',
-		'127.0.0.1/32',
+		'v4' => array(
+			'10.0.0.0/8',
+			'100.64.0.0/10',
+			'172.16.0.0/12',
+			'192.168.0.0/16',
+			'127.0.0.1/32',
+		),
+		'v6' => array(
+			'0:0:0:0:0:0:0:1/128', // localhost
+			'0:0:0:0:0:0:a:1/128', // ::ffff:127.0.0.1
+		),
 	);
 	
 	/*
 	*	Getting arrays of IP (REMOTE_ADDR, X-Forwarded-For, X-Real-Ip, Cf_Connecting_Ip)
 	*	reutrns array('remote_addr' => 'val', ['x_forwarded_for' => 'val', ['x_real_ip' => 'val', ['cloud_flare' => 'val']]])
 	*/
-	static public function ip_get($ips_input = array('real', 'remote_addr', 'x_forwarded_for', 'x_real_ip', 'cloud_flare'), $v4_only = true)
+	static public function ip_get($ip_types = array('real', 'remote_addr', 'x_forwarded_for', 'x_real_ip', 'cloud_flare'), $v4_only = true)
 	{
-		$ips = array();
-		foreach($ips_input as $ip_type){
-			$ips[$ip_type] = '';
-		} unset($ip_type);
-				
+		$ips = array_flip($ip_types);				
+
 		$headers = function_exists('apache_request_headers') ? apache_request_headers() : self::apache_request_headers();
 		
 		// REMOTE_ADDR
 		if(isset($ips['remote_addr'])){
-			$ips['remote_addr'] = $_SERVER['REMOTE_ADDR'];
+			$ip_type = self::ip_validate($_SERVER['REMOTE_ADDR']);
+			if($ip_type){
+				$ips['remote_addr'] = $ip_type == 'v6' ? self::ip_v6_normalize($_SERVER['REMOTE_ADDR']) : $_SERVER['REMOTE_ADDR'];
+			}			
 		}
 		
 		// X-Forwarded-For
 		if(isset($ips['x_forwarded_for'])){
 			if(isset($headers['X-Forwarded-For'])){
 				$tmp = explode(",", trim($headers['X-Forwarded-For']));
-				$ips['x_forwarded_for']= trim($tmp[0]);
+				$tmp = trim($tmp[0]);
+				$ip_type = self::ip_validate($tmp);
+				if($ip_type){
+					$ips['x_forwarded_for'] = $ip_type == 'v6' ? self::ip_v6_normalize($tmp) : $tmp;
+				}
 			}
 		}
 		
 		// X-Real-Ip
 		if(isset($ips['x_real_ip'])){
 			if(isset($headers['X-Real-Ip'])){
-				$tmp = explode(",", trim($headers['X-Real-Ip']));
-				$ips['x_real_ip']= trim($tmp[0]);
+				$tmp = explode(",", trim($headers['X-Forwarded-For']));
+				$tmp = trim($tmp[0]);
+				$ip_type = self::ip_validate($tmp);
+				if($ip_type){
+					$ips['x_forwarded_for'] = $ip_type == 'v6' ? self::ip_v6_normalize($tmp) : $tmp;
+				}
 			}
 		}
 		
 		// Cloud Flare
 		if(isset($ips['cloud_flare'])){
-			if(isset($headers['Cf-Connecting-Ip'])){
-				if(self::ip_mask_match($ips['remote_addr'], self::$cdn_pool['cloud_flare']['ipv4'])){
-					$ips['cloud_flare'] = $headers['Cf-Connecting-Ip'];
+			if(isset($headers['Cf-Connecting-Ip'], $headers['Cf-Ipcountry'], $headers['Cf-Ray'])){
+				$ip_type = self::ip_validate($_SERVER['REMOTE_ADDR']);
+				if($ip_type){
+//					if(self::ip_mask_match($ips['remote_addr'], self::$cdn_pool['cloud_flare']['ipv4'])){
+						$ips['cloud_flare'] = $headers['Cf-Connecting-Ip'];
 				}
 			}
 		}
 		
 		// Getting real IP from REMOTE_ADDR or Cf_Connecting_Ip if set or from (X-Forwarded-For, X-Real-Ip) if REMOTE_ADDR is local.
 		if(isset($ips['real'])){
-			
-			$ips['real'] = $_SERVER['REMOTE_ADDR'];
+
+			$ip_type = self::ip_validate($_SERVER['REMOTE_ADDR']);
+
+			if($ip_type)
+				$ips['real'] = $ip_type == 'v6' ? self::ip_v6_normalize($_SERVER['REMOTE_ADDR']) : $_SERVER['REMOTE_ADDR'];
 			
 			// Cloud Flare
-			if(isset($headers['Cf-Connecting-Ip'])){
-				if(self::ip_mask_match($ips['real'], self::$cdn_pool['cloud_flare']['ipv4'])){
-					$ips['real'] = $headers['Cf-Connecting-Ip'];
-				}
+			if(isset($headers['Cf-Connecting-Ip'], $headers['Cf-Ipcountry'], $headers['Cf-Ray'])){
+				$ip_type = self::ip_validate($headers['Cf-Connecting-Ip']);
+				if($ip_type)
+					$ips['real'] = $ip_type == 'v6' ? self::ip_v6_normalize($headers['Cf-Connecting-Ip']) : $headers['Cf-Connecting-Ip'];
+				
+			// Sucury
+			}elseif(isset($headers['X-Sucuri-Clientip'], $headers['X-Sucuri-Country'])){
+				$ip_type = self::ip_validate($headers['X-Sucuri-Clientip']);
+				if($ip_type)
+					$ips['real'] = $ip_type == 'v6' ? self::ip_v6_normalize($headers['X-Sucuri-Clientip']) : $headers['X-Sucuri-Clientip'];
+				
+			// OVH
+			}elseif(isset($headers['X-Cdn-Any-Ip'], $headers['Remote-Ip'])){
+				$ip_type = self::ip_validate($headers['X-Cdn-Any-Ip']);
+				if($ip_type)
+					$ips['real'] = $ip_type == 'v6' ? self::ip_v6_normalize($headers['X-Cdn-Any-Ip']) : $headers['X-Cdn-Any-Ip'];
+			
 			// Incapsula proxy
 			}elseif(isset($headers['Incap-Client-Ip'])){
-				$ips['real'] = $headers['Incap-Client-Ip'];
-			// Private networks. Looking for X-Forwarded-For and X-Real-Ip
-			}elseif(self::ip_mask_match($ips['real'], self::$private_networks)){
+				$ip_type = self::ip_validate($headers['Incap-Client-Ip']);
+				if($ip_type)
+					$ips['real'] = $ip_type == 'v6' ? self::ip_v6_normalize($headers['Incap-Client-Ip']) : $headers['Incap-Client-Ip'];
+			}
+
+			// Is private network
+			if($ip_type === false || ($ip_type && (self::ip_is_private_network($ips['real'], $ip_type)) || (self::ip_mask_match($ips['real'], filter_input(INPUT_SERVER, 'SERVER_ADDR').'/24', $ip_type)))){
+				
+				// X-Forwarded-For
 				if(isset($headers['X-Forwarded-For'])){
 					$tmp = explode(",", trim($headers['X-Forwarded-For']));
-					$ips['real']= trim($tmp[0]);
+					$tmp = trim($tmp[0]);
+					$ip_type = self::ip_validate($tmp);
+					if($ip_type)
+						$ips['real'] = $ip_type == 'v6' ? self::ip_v6_normalize($tmp) : $tmp;
+				
+				// X-Real-Ip
 				}elseif(isset($headers['X-Real-Ip'])){
 					$tmp = explode(",", trim($headers['X-Real-Ip']));
-					$ips['real']= trim($tmp[0]);
+					$tmp = trim($tmp[0]);
+					$ip_type = self::ip_validate($tmp);
+					if($ip_type)
+						$ips['real'] = $ip_type == 'v6' ? self::ip_v6_normalize($tmp) : $tmp;
 				}
-			}
+			}			
 		}
 		
 		// Validating IPs
 		$result = array();
 		foreach($ips as $key => $ip){
-			if($v4_only){
-				if(self::ip_validate($ip) == 'v4')
-					$result[$key] = $ip;
-			}else{
-				if(self::ip_validate($ip))
-					$result[$key] = $ip;
+			$ip_version = self::ip_validate($ip);
+			if($ip && (($v4_only && $ip_version == 'v4') || !$v4_only)){
+				$result[$key] = $ip;
 			}
 		}
 		
 		$result = array_unique($result);
-		
-		return count($ips_input) > 1 
-			? $result 
+		return count($result) > 1 
+			? $result
 			: (reset($result) !== false
 				? reset($result)
 				: null);
@@ -154,18 +196,53 @@ class CleantalkHelper
 	 * @param cird mixed (string|array of strings)
 	*/
 	static public function ip_mask_match($ip, $cidr){
+
 		if(is_array($cidr)){
 			foreach($cidr as $curr_mask){
-				if(self::ip_mask_match($ip, $curr_mask)){
+				if(self::ip_mask_match($ip, $curr_mask, $ip_type)){
 					return true;
 				}
 			} unset($curr_mask);
 			return false;
 		}
-		$exploded = explode ('/', $cidr);
-		$net = $exploded[0];
-		$mask = 4294967295 << (32 - $exploded[1]);
-		return (ip2long($ip) & $mask) == (ip2long($net) & $mask);
+		
+		if($ip_type == 'v4') $xtet_base = 8;
+		if($ip_type == 'v6') $xtet_base = 16;
+		
+		// Calculate mask
+		$exploded = explode('/', $cidr);
+		$net_ip = $exploded[0];
+		$mask   = $exploded[1];
+		
+		// Exit condition
+		$xtet_end = ceil($mask / $xtet_base);
+		if($xtet_count == $xtet_end)
+			return true;
+		
+		// Lenght of bits for comparsion
+		$mask = $mask - $xtet_base * $xtet_count >= $xtet_base ? $xtet_base : $mask - $xtet_base * $xtet_count;
+		
+		// Explode by octets/hextets from IP and Net
+		$net_ip_xtets = explode($ip_type == 'v4' ? '.' : ':', $net_ip);
+		$ip_xtets     = explode($ip_type == 'v4' ? '.' : ':', $ip);
+		
+		// Standartizing. Getting current octets/hextets. Adding leading zeros. 
+		$net_xtet = str_pad(decbin($ip_type == 'v4' ? $net_ip_xtets[$xtet_count]  : hexdec($net_ip_xtets[$xtet_count])), $xtet_base, 0, STR_PAD_LEFT);
+		$ip_xtet  = str_pad(decbin($ip_type == 'v4' ? $ip_xtets[$xtet_count]      : hexdec($ip_xtets[$xtet_count])),     $xtet_base, 0, STR_PAD_LEFT);
+		
+		// Comparing bit by bit
+		for($i = 0, $result = true; $mask != 0; $mask--, $i++ ){
+			if($ip_xtet[$i] != $net_xtet[$i]){
+				$result = false;
+				break;
+			}
+		}
+		
+		// Recursing. Moving to next octet/hextet.
+		if($result)
+			$result = self::ip_mask_match($ip, $cidr, $ip_type, $xtet_count + 1);
+		
+		return $result;
 	}
 	
 	/*
@@ -175,12 +252,52 @@ class CleantalkHelper
 	*/
 	static public function ip_validate($ip)
 	{
-		if(!$ip)                                                  return false; // NULL || FALSE || '' || so on...
-		if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) return 'v4';  // IPv4
-		if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) return 'v6';  // IPv6
-		                                                          return false; // Unknown
+		if(!$ip)                                                                                        return false; // NULL || FALSE || '' || so on...
+		if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) && $ip != '0.0.0.0')                   return 'v4';  // IPv4
+		if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) && self::ip_v6_reduce($ip) != '0::0')  return 'v6';  // IPv6
+		                                                                                                return false; // Unknown
 	}
-	
+
+	/**
+	 * Reduce IPv6
+	 * param (string) $ip
+	 * returns (string) IPv6
+	 */
+	static public function ip_v6_reduce($ip){
+		if(strpos($ip, ':') !== false){
+			$ip = preg_replace('/:0{1,4}/', ':',  $ip);
+			$ip = preg_replace('/:{2,}/',   '::', $ip);
+			$ip = strpos($ip, '0') === 0 ? substr($ip, 1) : $ip;
+		}
+		return $ip;
+	}	
+
+	/**
+	 * Expand IPv6
+	 * param (string) $ip
+	 * returns (string) IPv6
+	 */
+	static public function ip_v6_normalize($ip)
+	{
+		$ip = trim($ip);
+		// Searching for ::ffff:xx.xx.xx.xx patterns and turn it to IPv6
+		if(preg_match('/^::ffff:([0-9]{1,3}\.?){4}$/', $ip)){
+			$ip = dechex(sprintf("%u", ip2long(substr($ip, 7))));
+			$ip = '0:0:0:0:0:0:'.(strlen($ip) > 4 ? substr('abcde', 0, -4) : '0').':'.substr($ip, -4, 4);
+		// Normalizing hextets number
+		}elseif(strpos($ip, '::') !== false){
+			$ip = str_replace('::', str_repeat(':0', 8 - substr_count($ip, ':')).':', $ip);
+			$ip = strpos($ip, ':') === 0 ? '0'.$ip : $ip;
+			$ip = strpos(strrev($ip), ':') === 0 ? $ip.'0' : $ip;
+		}
+		// Simplifyng hextets
+		if(preg_match('/:0(?=[a-z0-9]+)/', $ip)){
+			$ip = preg_replace('/:0(?=[a-z0-9]+)/', ':', strtolower($ip));
+			$ip = self::ip_v6_normalize($ip);
+		}
+		return $ip;
+	}
+
 	/*
 	* Wrapper for sfw_logs API method
 	* 
@@ -218,7 +335,11 @@ class CleantalkHelper
 		
 		return $result;
 	}
-	
+
+	static function ip_is_private_network($ip, $ip_type = 'v4'){
+		return self::ip_mask_match($ip, self::$private_networks[$ip_type]);
+	}
+
 	/**
 	 * Function gets access key automatically
 	 *
