08e43e4e0f929d662a62fabdd89b3d69fb3b421a
Artem Davydov <yozzeebeatz@gmail.com>
2021-08-04 14:51:33 +0500

    Fix: empty string offset warning.

--- src/CleantalkHelper.php
+++ src/CleantalkHelper.php
@@ -644,8 +644,10 @@ class CleantalkHelper
 				$key_parts = explode('_', $server_key);
 				if(count($key_parts) > 0 and strlen($server_key) > 2){
 					foreach($key_parts as $part_index => $part){
-						$key_parts[$part_index] = mb_strtolower($part);
-						$key_parts[$part_index][0] = strtoupper($key_parts[$part_index][0]);					
+						if (!empty($part)) {
+							$key_parts[$part_index] = mb_strtolower($part);
+							$key_parts[$part_index][0] = strtoupper($key_parts[$part_index][0]);
+						}
 					}
 					$server_key = implode('-', $key_parts);
 				}
